314e8880699662b85bbf08b257fb27b94b383df9
Pedro Cruz <pedrocruz657@gmail.com>
2018-01-28 18:57:02 -0600

    Improvements in code

--- boot/compile.sh
+++ boot/compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 as bootloader.S -o bootloader.o
-ld -Ttext 0x7c00 --oformat=binary bootloader.o -o bootloader.bin
+ld -T boot.ld
 dd if=/dev/zero of=floppy.img bs=1024 count=1440
 dd if=bootloader.bin of=floppy.img seek=0 count=1 conv=notrunc
 cp floppy.img iso/

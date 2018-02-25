4063e1900ec2141254f952f049f939ced1beafce
Pedro Cruz <pedrocruz657@gmail.com>
2018-02-25 10:54:44 -0600

    Change to gcc compiler

--- boot/compile.sh
+++ boot/compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-as bootloader.S -o bootloader.o
+gcc -c bootloader.S
 ld -T boot.ld
 dd if=/dev/zero of=floppy.img bs=1024 count=1440
 dd if=bootloader.bin of=floppy.img seek=0 count=1 conv=notrunc

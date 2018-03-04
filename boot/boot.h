c0eda3fcb27a441030d22b93cef49ced7d1e8739
Pedro Cruz <pedrocruz657@gmail.com>
2018-03-04 15:39:30 -0600

    Setup stack added

--- boot/boot.h
+++ boot/boot.h
@@ -1,3 +1,7 @@
 #define SETUP_BUFFER_SEG 0x1000
 
+#define SETUP_STACK_SIZE 0x400
+
 #define halt_cpu() cli; hlt 
+
+#define print(x) pushw $x; call print_msg
--- boot/compile.sh
+++ boot/compile.sh
@@ -6,6 +6,6 @@ ld -T boot.ld
 ld -T setup.ld
 dd if=/dev/zero of=floppy.img bs=1024 count=1440
 dd if=bootloader.bin of=floppy.img seek=0 count=1 conv=notrunc
-dd if=setup.bin of=floppy.img seek=1 count=1 conv=notrunc
+dd if=setup.bin of=floppy.img seek=1 conv=notrunc
 cp floppy.img iso/
 genisoimage -quiet -V 'TAURUS' -input-charset iso8859-1 -o myos.iso -b floppy.img -hide floppy.img iso/
